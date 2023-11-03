310d62bb7079be8f4204a514174c5bd8b1072056
Sarthak Niranjan <22b3923@iitb.ac.in>
2023-11-03 22:20:23 +0530

    add swtitchTabs functionality

--- src/pages/home/trending/Trending.js
+++ src/pages/home/trending/Trending.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import React from "react";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import { useState } from "react";
 
 const Trending = () => {
+  //a function to handle tab change behaviour
+  const onTabChange = (tab) => {};
   return (
-    <div className='carouselSection'>
-        <ContentWrapper>
-            <span className="carouselTitle">Trending</span>
-        </ContentWrapper>
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        {/* passing all the relevant time frames of trending to switchTabs component */}
+        <SwitchTabs data={["Day", "Week", "Month"]} onTabChange={onTabChange} />
+      </ContentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
